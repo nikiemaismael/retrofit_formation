7c58df49eefae6ab5ea470b83168ea488461bc42
Ismael <nikiemaismael2020@gmail.com>
2025-09-22 08:20:18 +0000

    Feat: Implement network request and add logging for posts
    - Added INTERNET and ACCESS_NETWORK_STATE permissions to AndroidManifest.xml.
    - Enabled NSAllowsArbitraryLoads in Info.plist for iOS to allow HTTP requests.
    - Added a debug log for the fetched posts in PostViewModel.

--- lib/view_model/post_view_model.dart
+++ lib/view_model/post_view_model.dart
@@ -14,6 +14,7 @@ class PostViewModel extends ChangeNotifier {
     try {
       final posts = await restClient.getPosts();
       this.posts = posts;
+      logger.d(posts);
     } catch (e) {
       logger.e(e);
     }finally {
